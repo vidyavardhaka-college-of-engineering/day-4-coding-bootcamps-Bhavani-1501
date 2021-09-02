de48c77e80cf6b5dcbc5fc0850bea2a24ec9257f
Bhavani1501 <81621425+Bhavani-1501@users.noreply.github.com>
2021-09-02 14:11:37 +0000

    Done

--- Program_3.c
+++ Program_3.c
@@ -6,10 +6,33 @@
 //Output: 
 //1   2
 //3   4
-#include<stdio.h> 
+#include<stdio.h>
 int main()
 {
-  //Declare all the variables[row, col, array a(2D array), i] used in the code
+int disp[2][2];
+int i, j;
+for(i=0; i<2; i++)
+{
+for(j=0;j<2;j++) 
+{
+printf("Enter value for disp[%d][%d]:", i, j);
+scanf("%d", &disp[i][j]);
+}
+}
+printf("Two Dimensional array elements:\n");
+for(i=0; i<2; i++) 
+{
+for(j=0;j<2;j++) 
+{
+printf("%d ", disp[i][j]);
+if(j==1)
+{
+printf("\n");
+}
+}
+}
+  
+     //Declare all the variables[row, col, array a(2D array), i] used in the code
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
    //Read the elements of matrix a using nested for loop and scanf statment
    //Print the elements of matrix a using nested for loop and printf statment as per the above specified format
